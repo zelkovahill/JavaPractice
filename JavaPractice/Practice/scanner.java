1f2608323ec941f2e0eaae12007966e17b14458a
KAWAI~ WOOMINCHAN~ <changwoomin426@gmail.com>
2024-07-09 19:41:16 +0900

    자바 터미널 실험

---
+++ JavaPractice/Practice/scanner.java
@@ -0,0 +1,17 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class scanner
+{
+    public static void main(String[] args)
+    {
+        int n;        
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+        System.out.println("You entered: " + n);
+
+    }
+    
+}
